977911f3a6282b9ffd9245a1455cdced7cb8a1d0
jon-cron <jonathanacm95@gmail.com>
2023-02-17 10:48:48 -0700

    push input fields

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import {Text, Input, Button} from 'react-native-elements'
 // NOTE this is a utility class that provides margin to where ever it is place
 import Spacer from "../components/Spacer.js";
 
 const SignUpScreen = ({navigation}) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
 // NOTE now using react-native-elements imports
 return (
   <View style={styles.container}>
   <Spacer>
     <Text h3>Sign Up for Tracker</Text>
   </Spacer>
-    <Input label="Email"/>
+    <Input
+      autoCapitalize="none"
+      autoCorrect={false}
+      label="Email"
+      value={email}
+      // onChangeText={(newEmail)=> setEmail(newEmail)}/>
+      // NOTE or we can condense this statement down to this...
+      onChangeText={setEmail}/>
   <Spacer/>
-    <Input label="Password"/>
+    <Input 
+      autoCapitalize="none"
+      autoCorrect={false}
+      secureTextEntry
+      label="Password"
+      value={password}
+      onChange={setPassword}/>
     <Spacer>
     <Button title="SignUp"/>
     </Spacer>
