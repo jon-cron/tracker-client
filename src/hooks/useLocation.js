62aa3ad84b852be5c59b6207ef4cfdcffc590da6
jon-cron <jonathanacm95@gmail.com>
2023-02-18 13:49:15 -0700

    refactoring

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,2 +1,35 @@
 import {useState, useEffect} from 'react'
 import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+
+
+export default (callback) => {
+  const [error, setError] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      await watchPositionAsync({
+        // NOTE this can be altered to save the users battery life but if you need very specific location for your app make it very accurate as seen here 
+        accuracy: Accuracy.BestForNavigation,
+        // NOTE how often we are updating the users location in milliseconds
+        timeInterval: 1000,
+        // NOTE this tells expo to update the location if the user travels 10 meters before the timeInterval is met
+        distanceInterval: 10
+      }, (location) => {
+        // NOTE this is taking the users current location and sending it to the context through this function
+        callback
+      })
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(()=> {
+    startWatching();
+  }, [])
+
+  return [error]
+}
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,43 +1,20 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet } from 'react-native';
 import { Text } from "react-native-elements";
 import Map from "../components/Map.js";
 import { SafeAreaView } from "react-navigation";
 import Spacer from "../components/Spacer.js";
-import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import '../_mockLocation.js'
 import { Context as LocationContext } from "../context/LocationContext.js";
+import useLocation from "../hooks/useLocation.js";
 
 
 const TrackCreateScreen = () => {
   const {addLocation} = useContext(LocationContext)
-  const [error, setError] = useState(null)
+
+const [error] = useLocation((location) => addLocation(location))
 
 
-const startWatching = async () => {
-  try {
-    const { granted } = await requestForegroundPermissionsAsync();
-    await watchPositionAsync({
-      // NOTE this can be altered to save the users battery life but if you need very specific location for your app make it very accurate as seen here 
-      accuracy: Accuracy.BestForNavigation,
-      // NOTE how often we are updating the users location in milliseconds
-      timeInterval: 1000,
-      // NOTE this tells expo to update the location if the user travels 10 meters before the timeInterval is met
-      distanceInterval: 10
-    }, (location) => {
-      // NOTE this is taking the users current location and sending it to the context through this function
-      addLocation(location)
-    })
-    if (!granted) {
-      throw new Error('Location permission not granted');
-    }
-  } catch (error) {
-    setError(error);
-  }
-};
-useEffect(()=> {
-  startWatching();
-}, [])
 
 return (
   <SafeAreaView forceInset={{top: "always"}}>
