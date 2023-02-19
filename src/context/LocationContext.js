a3f4317f2be2e86a986e22ecae1949e60c6edd87
jon-cron <jonathanacm95@gmail.com>
2023-02-19 11:52:49 -0700

    push changing name state and adding locations

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -9,13 +9,19 @@ const locationReducer = (state, action) => {
       return {...state, recording: true}
     case 'stop-recording':
       return {...state, recording: false}
+    case 'add_location':
+      return {...state, locations: [...state.locations, action.payload]}
+    case 'change_name':
+      return {...state, name: action.payload}
     default:
       return state
   }
 }
 
 // SECTION action functions
-
+const changeName = dispatch => (name) => {
+  dispatch({type: 'change_name', payload: name})
+}
 const startRecording = dispatch => () => {
   dispatch({type: 'start_recording'})
 }
@@ -24,11 +30,16 @@ const stopRecording = dispatch => () => {
 }
 const addLocation = dispatch => (location, recording) => {
   // console.log('adding location')
-dispatch({ type: 'add_current_location', payload: location})
+dispatch({ type: 'add_current_location', payload: location});
+if(recording){
+  dispatch({type: 'add_location', payload: location})
+}
 }
 
 export const {Context, Provider} = createDataContext(
   locationReducer,
-  {startRecording, stopRecording, addLocation},
-  {recording: false, locations: [], currentLocation: null}
+  // NOTE these are the functions we export so that other files can have access
+  {startRecording, stopRecording, addLocation, changeName},
+  // NOTE this section are the types of state we use to change through the functions above
+  {name: '', recording: false, locations: [], currentLocation: null}
 )
