004f907d9add3976d756c7681b4dd1197ed97b12
jon-cron <jonathanacm95@gmail.com>
2023-02-20 11:58:09 -0700

    automatically authenticating on instance

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -9,7 +9,18 @@ const instance = axios.create({
   baseURL: 'http://8fa9-75-174-192-122.ngrok.io'
 });
 
-
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token')
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+)
 
 
 
